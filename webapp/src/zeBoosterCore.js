02fd24036e01c5279560f9f8014e923551d8cdf9
Maksym Pavlov <Max_account@maksyms-mbp-2.budapest.epam.com>
2016-06-28 11:22:05 +0200

    extracted speedometer. Added jQuery import

--- webapp/src/zeBoosterCore.js
+++ webapp/src/zeBoosterCore.js
@@ -15,7 +15,7 @@ var zeBoosterCore = {
         this.analyser.minDecibels = -90;
         this.analyser.maxDecibels = -10;
         this.analyser.smoothingTimeConstant = 0.85;
-        this.speedometr.init();
+        speedometr.init();
     },
 
     start: function () {
@@ -37,7 +37,7 @@ var zeBoosterCore = {
         this.animationID = window.setInterval(function () {
             var v = self.oscillator.frequency.value;
             $(".current-frequency p").html(parseInt(self.oscillator.frequency.value) + 'Hz');
-            zeBoosterCore.speedometr.setStatValue((v / 5).toFixed(1));
+            speedometr.setStatValue((v / 5).toFixed(1));
         }, 50);
     },
 
@@ -53,59 +53,6 @@ var zeBoosterCore = {
 };
 
 
-zeBoosterCore.speedometr = {};
-zeBoosterCore.speedometr.init = function () {
-    this.ticks = $('.tick');
-    this.digits = $('.digit');
-    this.details = $('.details');
-    this.progress = $('.progress');
-    this.outerRingRadius = 164;
-    this.digitRingRadius = 145;
-    this.digitValueMax = 160;
-    var self = this;
-    this.ticks.each(function (i) {
-        var angle = 210 - i * 5;
-        var theta = self.deg2rad(angle);
-        var radius = self.outerRingRadius + (i % 6 ? 0 : 4);
-        var x = Math.cos(theta) * radius;
-        var y = Math.sin(theta) * -radius;
-        var transform = [
-            'translate(' + x + 'px, ' + y + 'px)',
-            'rotate(' + -angle + 'deg)'
-        ].join(' ');
-        $(this).css({
-            '-webkit-transform': transform,
-            '-moz-transform': transform,
-            'transform': transform
-        });
-    });
-    this.digits.each(function (i) {
-        var angle = 210 - i * 30;
-        var theta = self.deg2rad(angle);
-        var x = Math.cos(theta) * self.digitRingRadius;
-        var y = Math.sin(theta) * -self.digitRingRadius;
-        $(this).css({
-            '-webkit-transform': 'translate(' + x + 'px, ' + y + 'px)',
-            '-moz-transform': 'translate(' + x + 'px, ' + y + 'px)',
-            'transform': 'translate(' + x + 'px, ' + y + 'px)'
-        });
-    });
-};
-
-zeBoosterCore.speedometr.setStatValue = function (value) {
-    var angle = -120 + 240 * (value / this.digitValueMax);
-    this.progress.css({
-        'transform': 'rotate(' + angle + 'deg)'
-    });
-    this.details.find('.speed').text((value * 100).toFixed(0));
-};
-
-zeBoosterCore.speedometr.deg2rad = function (angle) {
-    return angle * (Math.PI / 180);
-};
-zeBoosterCore.speedometr.rad2deg = function (angle) {
-    return angle * (180 / Math.PI);
-};
 
 zeBoosterCore.oscillograph = function () {
     var self = this;
--- webapp/src/zeBoosterInitializer.js
+++ webapp/src/zeBoosterInitializer.js
@@ -1,45 +1,50 @@
 'use strict';
-var zeBoosterInitializer = {
-    configureMouseWheel: function () {
-        var mouseRpm = 0;
-        $('.gas-pedal').on('mousewheel', function (e) {
-            mouseRpm += e.deltaY * 70;
-            if (mouseRpm < 0) mouseRpm = 0;
-            if (mouseRpm > 800) mouseRpm = 800;
-            zeBoosterCore.accelerate(mouseRpm);
-        });
-    },
+// jQuery
+$.getScript('src/zeBoosterCore.js', function () {
+    var zeBoosterInitializer = {
+        configureMouseWheel: function () {
+            var mouseRpm = 0;
+            $('.gas-pedal').on('mousewheel', function (e) {
+                mouseRpm += e.deltaY * 70;
+                if (mouseRpm < 0) mouseRpm = 0;
+                if (mouseRpm > 800) mouseRpm = 800;
+                zeBoosterCore.accelerate(mouseRpm);
+            });
+        },
 
-    // Will be replaced by hover off
-    // KISS principle will drive us to success :)
-    configureStopSound: function () {
-        $('.stop-pedal').on('click', function (e) {
-            zeBoosterCore.stop();
-        });
-    },
+        // Will be replaced by hover off
+        // KISS principle will drive us to success :)
+        configureStopSound: function () {
+            $('.stop-pedal').on('click', function (e) {
+                zeBoosterCore.stop();
+            });
+        },
 
-    //Will be replaced by accelerate/regeneration sound
-    configureSoundChanger: function () {
-        var waveNumbr = 0;
-        var waves = ['sine', 'square', 'sawtooth', 'triangle'];
-        $('.waveform').on('click', function (e) {
-            waveNumbr++;
-            if (waveNumbr > 3) waveNumbr = 0;
-            zeBoosterCore.oscillator.type = waves[waveNumbr];
-            $(this).html(waves[waveNumbr]);
-            console.log(this);
-        });
-    },
+        //Will be replaced by accelerate/regeneration sound
+        configureSoundChanger: function () {
+            var waveNumbr = 0;
+            var waves = ['sine', 'square', 'sawtooth', 'triangle'];
+            $('.waveform').on('click', function (e) {
+                waveNumbr++;
+                if (waveNumbr > 3) waveNumbr = 0;
+                zeBoosterCore.oscillator.type = waves[waveNumbr];
+                $(this).html(waves[waveNumbr]);
+                console.log(this);
+            });
+        },
 
-    init: function () {
-        zeBoosterInitializer.configureMouseWheel();
-        zeBoosterInitializer.configureStopSound();
-        zeBoosterInitializer.configureSoundChanger();
+        init: function () {
+            zeBoosterInitializer.configureMouseWheel();
+            zeBoosterInitializer.configureStopSound();
+            zeBoosterInitializer.configureSoundChanger();
 
-        zeBoosterCore.init();
-        zeBoosterCore.start();
-    }
-};
+            zeBoosterCore.init();
+            zeBoosterCore.start();
+        }
+    };
 
 // Entry point to start play with web audio toy :)
-$(document).ready(zeBoosterInitializer.init);
+    $(document).ready(zeBoosterInitializer.init);
+});
+
+
