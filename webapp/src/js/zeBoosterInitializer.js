1fcbc8746ecfe7f15f490e25d7e95e8442ed41ce
Maksym Pavlov <Max_account@Maksyms-MacBook-Pro-2.local>
2016-07-07 19:25:17 +0200

    refactored initializer. Introduce "Module pattern" https://learn.jquery.com/code-organization/concepts/

--- webapp/src/js/zeBoosterInitializer.js
+++ webapp/src/js/zeBoosterInitializer.js
@@ -1,45 +1,42 @@
 'use strict';
-var zeBoosterInitializer = {
-    init: function () {
+// Entry point to start play with web audio toy :)
+$(document).ready(function() {
+    zeBoosterInitializer.init()
+});
+
+var zeBoosterInitializer = (function () {
+    var init = function () {
+
+        configureMouseWheel();
+        configureActivationSound();
+        configureSoundChanger();
+
         zeBoosterCore.init();
         zeBoosterCore.start();
+    };
 
-        zeBoosterInitializer.configureMouseWheel();
-        zeBoosterInitializer.configureStopSound();
-        zeBoosterInitializer.configureSoundChanger();
-        zeBoosterInitializer.configureActivationSound();
-    },
-
-    configureMouseWheel: function () {
+    var configureMouseWheel = function () {
         var mouseRpm = 0;
-        $('.gas-pedal').on('mousewheel', function (e) {
-            mouseRpm += e.deltaY * 70;
+        $('.tachometer-container').on('mousewheel', function (e) {
+            mouseRpm += e.deltaY * 10;
             if (mouseRpm < 0) mouseRpm = 0;
             if (mouseRpm > 800) mouseRpm = 800;
             zeBoosterCore.accelerate(mouseRpm);
         });
-    },
+    };
 
-    configureActivationSound: function () {
+    var configureActivationSound = function () {
         $('.tachometer-container')
             .mouseover(function () {
-                zeBoosterCore.accelerate(150);
+                zeBoosterCore.accelerate(120);
             })
-            .mouseout(function(){
+            .mouseout(function () {
                 zeBoosterCore.stop()
             })
-    },
-
-    // Will be replaced by hover off
-    // KISS principle will drive us to success :)
-    configureStopSound: function () {
-        $('.stop-pedal').on('click', function (e) {
-            zeBoosterCore.stop();
-        });
-    },
+    };
 
     //Will be replaced by accelerate/regeneration sound
-    configureSoundChanger: function () {
+    var configureSoundChanger = function () {
         var waveNumbr = 0;
         var waves = ['sine', 'square', 'sawtooth', 'triangle'];
         $('.waveform').on('click', function (e) {
@@ -49,10 +46,9 @@ var zeBoosterInitializer = {
             $(this).html(waves[waveNumbr]);
             console.log(this);
         });
-    }
-};
+    };
 
-// Entry point to start play with web audio toy :)
-$(document).ready(zeBoosterInitializer.init);
+    return {init: init};
+})();
 
 
