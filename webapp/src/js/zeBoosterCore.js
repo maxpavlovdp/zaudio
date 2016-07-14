5a6f646c858a1cdace97249d45b3406ad9fabee4
Maksym Pavlov <Max_account@maksyms-mbp-2.budapest.epam.com>
2016-07-14 20:41:01 +0200

    added example. refactored core.

--- webapp/src/js/zeBoosterCore.js
+++ webapp/src/js/zeBoosterCore.js
@@ -1,30 +1,33 @@
 'use strict';
-var zeBoosterCore = {
-    init: function () {
-        //Depends on web browser
-        var audioContextClass = (window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.oAudioContext || window.msAudioContext);
-        this.webAudioContext = new audioContextClass();
+var zeBoosterCore = (function () {
+
+    //Depends on web browser
+    var webAudioContext;
+
+    var init = function () {
+        webAudioContext = new (window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.oAudioContext || window.msAudioContext);
+
         this.gainValue = 0.1;
         this.gainNode;
         this.oscillator;
         this.animationID;
         this.canvas = document.getElementById('visualizer');
         this.canvasContext = this.canvas.getContext('2d');
-        this.analyser = this.webAudioContext.createAnalyser();
+        this.analyser = webAudioContext.createAnalyser();
         this.analyser.minDecibels = -90;
         this.analyser.maxDecibels = -10;
         this.analyser.smoothingTimeConstant = 0.85;
         speedometer.init();
-    },
+    };
 
-    start: function () {
+    var start = function () {
         var self = this;
         if (typeof this.oscillator != 'undefined') this.oscillator.disconnect();
 
-        this.gainNode = this.webAudioContext.createGain();
-        this.gainNode.connect(this.webAudioContext.destination);
+        this.gainNode = webAudioContext.createGain();
+        this.gainNode.connect(webAudioContext.destination);
 
-        this.oscillator = this.webAudioContext.createOscillator();
+        this.oscillator = webAudioContext.createOscillator();
         this.oscillator.frequency.value = 0;
         this.oscillator.connect(this.gainNode);
         this.oscillator.type = 'sine';
@@ -38,15 +41,17 @@ var zeBoosterCore = {
             $(".current-frequency p").html(parseInt(self.oscillator.frequency.value) + 'Hz');
             speedometer.setStatValue((v / 5).toFixed(1));
         }, 50);
-    },
+    };
 
-    accelerate: function (gasVal) {
+    var accelerate = function (gasVal) {
         var duration = 0.1;
         // Change sound waves
-        this.oscillator.frequency.setTargetAtTime(gasVal, this.webAudioContext.currentTime + duration, 1.3);
-    },
+        this.oscillator.frequency.setTargetAtTime(gasVal, webAudioContext.currentTime + duration, 1.3);
+    };
 
-    stop: function () {
-        this.oscillator.frequency.setTargetAtTime(0, this.webAudioContext.currentTime + 0.1, 0.3);
-    }
-};
+    var stop = function () {
+        this.oscillator.frequency.setTargetAtTime(0, webAudioContext.currentTime + 0.1, 0.3);
+    };
+
+    return {init: init, start: start, accelerate: accelerate, stop: stop}
+})();
