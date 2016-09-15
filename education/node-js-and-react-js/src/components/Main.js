939b9070ec7cfd35a9bd23a047ae07f411f17e89
Maksym Pavlov <Max_account@xpplgdaw0112.budapest.epam.com>
2016-09-15 11:39:29 +0200

    BOOST-88 added contact search app

--- education/node-js-and-react-js/src/components/Main.js
+++ education/node-js-and-react-js/src/components/Main.js
@@ -3,6 +3,7 @@ require('styles/App.css');
 
 import React from 'react';
 import Edu from './zebooster/com/components/EduComponent';
+import ContactApp from './zebooster/com/components/ContactApp'
 
 
 let yeomanImage = require('../images/yeoman.png');
@@ -15,9 +16,8 @@ class AppComponent extends React.Component {
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
         <div>Edu component</div>
         <Edu/>
-
-
-
+        <div>Contact App</div>
+        <ContactApp/>
       </div>
     );
   }
