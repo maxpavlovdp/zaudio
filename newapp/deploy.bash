fda2c39935c3f98d7a2cba12d2f10dd0750b7fc0
Deploy user <maxpavlov.dp@gmail.com>
2016-10-20 00:24:19 +0200

    BOOST-100 deploy to github pages.

--- newapp/deploy.bash
+++ newapp/deploy.bash
@@ -3,6 +3,8 @@ set -x
 git config --global user.email "maxpavlov.dp@gmail.com"
 git config --global user.name "Deploy user"
 git config --global push.default simple
+git config --global credential.helper store
+ps: Add-Content "$env:USERPROFILE\.git-credentials" "https://264aac49eacabfceb4296bafff349ec4ddf076fe:x-oauth-basic@github.com`n"
 
 git clone --depth 1  https://github.com/maxpavlovdp/jetaudio.git
 cd ./jetaudio
