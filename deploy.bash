b7ed98b7c2da10cf94a17869ac45d003bb5ed326
maxpavlov.dp@gmail.com <maxpavlov.dp@gmail.com>
2016-10-19 22:46:23 +0200

    BOOST-100 deploy to github pages configuration.

--- deploy.bash
+++ deploy.bash
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+git clone https://github.com/maxpavlovdp/jetaudio.git
+cd ./jetaudio
+rm -rf *
+cp -r ../newapp/public/* .
+git add **
+git commit -m "deploy from bitbucket pipeline"
+git push
