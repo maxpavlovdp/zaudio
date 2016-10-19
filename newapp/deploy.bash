4b41f2942c65113e3ac7354c1b88c9f8ca3fa91d
Deploy user <maxpavlov.dp@gmail.com>
2016-10-20 01:00:44 +0200

    BOOST-100 deploy to github pages.

--- newapp/deploy.bash
+++ newapp/deploy.bash
@@ -7,12 +7,13 @@ git config --global push.default simple
 
 eval "$(ssh-agent -s)"
 ssh-add ./deploy
-echo -e "Host github.com (192.30.253.112)\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-git clone --depth 1  git@github.com:maxpavlovdp/jetaudio.git
-
-cd ./jetaudio
-rm -rf *
-cp -r ../public/* .
-git add **
-git commit -m "deploy from bitbucket pipeline"
-git push
+ls -la ~/.ssh/
+#echo -e "Host github.com (192.30.253.112)\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+#git clone --depth 1  git@github.com:maxpavlovdp/jetaudio.git
+#
+#cd ./jetaudio
+#rm -rf *
+#cp -r ../public/* .
+#git add **
+#git commit -m "deploy from bitbucket pipeline"
+#git push
