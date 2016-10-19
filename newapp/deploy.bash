da2b52dc45a2b4409bd3a5c30abfff94ad051f5e
Deploy user <maxpavlov.dp@gmail.com>
2016-10-20 00:46:14 +0200

    BOOST-100 deploy to github pages.

--- newapp/deploy.bash
+++ newapp/deploy.bash
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -x
+
 git config --global user.email "maxpavlov.dp@gmail.com"
 git config --global user.name "Deploy user"
 git config --global push.default simple
-git config --global credential.helper store
-ps: Add-Content "$env:USERPROFILE\.git-credentials" "https://264aac49eacabfceb4296bafff349ec4ddf076fe:x-oauth-basic@github.com"
 
-git clone --depth 1  https://github.com/maxpavlovdp/jetaudio.git
+eval "$(ssh-agent -s)"
+ssh-add ./deploy
+git clone --depth 1  git@github.com:maxpavlovdp/jetaudio.git
+
 cd ./jetaudio
 rm -rf *
 cp -r ../public/* .
