129fd1ad282febaaceb3829172a2bdf30fc78012
Deploy user <maxpavlov.dp@gmail.com>
2016-11-09 19:33:45 +0100

    BOOST-103 added sound component versions

--- newapp/deploy-to-git-hub-pages/deploy.bash
+++ newapp/deploy-to-git-hub-pages/deploy.bash
@@ -11,10 +11,9 @@ mkdir -p ~/.ssh
 touch ~/.ssh/known_hosts
 chmod 600 ./deploy_rsa
 ssh-add ./deploy_rsa
-
 ssh-keyscan github.com >> ~/.ssh/known_hosts
-git clone git@github.com:maxpavlovdp/jetaudio.git
 
+git clone --depth=1 git@github.com:maxpavlovdp/jetaudio.git
 cd ./jetaudio
 rm -rf *
 cp -r ../../dist/* .
--- newapp/src/components/ZEBoosterApp.jsx
+++ newapp/src/components/ZEBoosterApp.jsx
@@ -3,73 +3,73 @@ import React from 'react';
 import CarSimulator from './CarSimulator.jsx'
 import CarSoundEngine from './CarSoundEngine.js'
 
-let acceleration                 =  require("../sound/acceleration.ogg"),
-    engineOn                     =  require("../sound/engineOn.ogg"),
-    acceleration100FunLoop       =  require("../sound/acceleration100+FunLoop.ogg"),
-    acceleration160CrazyLoop     =  require("../sound/acceleration160+CrazyLoop.ogg"),
-    accelerationLoop             =  require("../sound/accelerationLoop.ogg"),
-    engineOffSample              =  require("../sound/engineOffSample.ogg"),
-    engineOnSample               =  require("../sound/engineOnSample.ogg"),
-    idlingLoop                   =  require("../sound/idlingLoop.ogg"),
-    recuperationLoop             =  require("../sound/recuperationLoop.ogg");
+let acceleration = require("../sound/acceleration.ogg"),
+    engineOn = require("../sound/engineOn.ogg"),
+    acceleration100FunLoop = require("../sound/acceleration100+FunLoop.ogg"),
+    acceleration160CrazyLoop = require("../sound/acceleration160+CrazyLoop.ogg"),
+    accelerationLoop = require("../sound/accelerationLoop.ogg"),
+    engineOffSample = require("../sound/engineOffSample.ogg"),
+    engineOnSample = require("../sound/engineOnSample.ogg"),
+    idlingLoop = require("../sound/idlingLoop.ogg"),
+    recuperationLoop = require("../sound/recuperationLoop.ogg");
 
 
 var car1 = {
-        start : {
-            link: engineOnSample
+    start: {
+        link: engineOnSample
+    },
+    main: [
+        {
+            link: accelerationLoop,
+            speed: {
+                margins: [0, 250]
+            },
+            defMargins: [0, 30],
+            loop: true
         },
-        main : [
-            {
-                link: accelerationLoop,
-                speed: {
-                    margins: [0, 250]
-                },
-                defMargins: [0, 30],
-                loop: true
+        {
+            link: idlingLoop,
+            speed: {
+                margins: [0, 250],
+                volume: [[0, 0.4], [150, 1]]
             },
-            {
-                link: idlingLoop,
-                speed: {
-                    margins: [0, 250],
-                    volume: [[0, 0.4], [150, 1]]
-                },
-                loop: true
+            loop: true
+        },
+        {
+            link: acceleration100FunLoop,
+            speed: {
+                margins: [100, 250],
+                volume: [[100, 0.01], [150, 1]]
             },
-            {
-                link: acceleration100FunLoop,
-                speed: {
-                    margins: [100, 250],
-                    volume: [[100, 0.01], [150, 1]]
-                },
-                loop: true
+            loop: true
+        },
+        {
+            link: acceleration160CrazyLoop,
+            speed: {
+                margins: [160, 250],
+                volume: [[160, 0.01], [170, 2]]
             },
-            {
-                link: acceleration160CrazyLoop,
-                speed: {
-                    margins: [160, 250],
-                    volume: [[160, 0.01], [170, 2]]
-                },
-                loop: true
+            loop: true
+        },
+        {
+            link: recuperationLoop,
+            speed: {
+                margins: [0, 250]
             },
-            {
-                link: recuperationLoop,
-                speed: {
-                    margins: [0, 250]
-                },
-                recuperation: true,
-                loop: true
-            }
-        ],
-        stop : {
-            link: engineOffSample
+            recuperation: true,
+            loop: true
         }
+    ],
+    stop: {
+        link: engineOffSample
+    }
 };
 
 var car2 = {
-    start : {
+    start: {
         link: engineOn
     },
-    main : [
+    main: [
         {
             link: acceleration,
             speed: {
@@ -86,11 +86,13 @@ class ZEB extends React.Component {
         super(props);
     }
 
-    render () {
-        return  <div>
-                    <CarSimulator soundgen={new CarSoundEngine(car1)}/>
-                    <CarSimulator soundgen={new CarSoundEngine(car2)}/>
-                </div>;
+    render() {
+        return <div>
+            <h1>V2</h1>
+            <CarSimulator soundgen={new CarSoundEngine(car1)}/>
+            <h1>V1</h1>
+            <CarSimulator soundgen={new CarSoundEngine(car2)}/>
+        </div>;
     }
 }
 
