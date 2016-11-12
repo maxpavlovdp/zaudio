7f29f2ea0b57cbed54430192b60572476ae84088
Max Pavlov <maxpavlov.dp@gmail.com>
2016-11-13 00:18:17 +0100

    BOOST-103 revert

--- webapp/deploy-to-git-hub-pages/deploy.bash
+++ webapp/deploy-to-git-hub-pages/deploy.bash
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
 set -x
 
+cd "$(dirname "$0")"
+
+eval "$(ssh-agent -s)"
+
+# Configure github pages access
+cd "$(dirname "$0")"
 git config --global user.email "maxpavlov.dp@gmail.com"
 git config --global user.name "Deploy user"
 git config --global push.default simple
 
-eval "$(ssh-agent -s)"
-
 mkdir -p ~/.ssh
 touch ~/.ssh/known_hosts
-chmod 600 ./deploy
-ssh-add ./deploy
-ls ~/.ssh
+chmod 600 ./deploy_rsa
+ssh-add ./deploy_rsa
 ssh-keyscan github.com >> ~/.ssh/known_hosts
-git clone git@github.com:maxpavlovdp/jetaudio.git
 
+# Push new build to github pages
+git clone --depth=1 git@github.com:maxpavlovdp/jetaudio.git
 cd ./jetaudio
 rm -rf *
-cp -r ../public/* .
+cp -r ../../dist/* .
 git add **
 git commit -m "deploy from bitbucket pipeline"
 git push
---
+++ webapp/readme.txt
@@ -0,0 +1,30 @@
+webpack configuration is based on https://github.com/react-webpack-generators/generator-react-webpack#readme
+
+Usage
+
+The following commands are available in ze-booster project:
+
+# Start for development
+npm start # or
+npm run serve
+
+# Start the dev-server with the dist version
+npm run serve:dist
+
+# Just build the dist version and copy static files
+npm run dist
+
+# Run unit tests
+npm test
+
+# Auto-run unit tests on file changes
+npm run test:watch
+
+# Lint all files in src (also automatically done AFTER tests are run)
+npm run lint
+
+# Clean up the dist directory
+npm run clean
+
+# Just copy the static assets to dist dir
+npm run copy
