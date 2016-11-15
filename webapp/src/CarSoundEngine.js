725fd7bf6ec8f36443fd338f111e76dfddf69231
Max Pavlov <maxpavlov.dp@gmail.com>
2016-11-15 22:10:38 +0100

    BOOST-122 added start/stop console log debug status.

--- webapp/src/CarSoundEngine.js
+++ webapp/src/CarSoundEngine.js
@@ -82,11 +82,13 @@ class CarSoundEngine {
                 afterSrartSounds.forEach((sound) => {
                     sound.gainNode.gain.value = 0;
                     sound.audioSource.start();
+                    console.log("start " + sound.link)
                     sound.started = true;
                 });
                 resolve();
             });
             this.config.start.audioSource.start();
+            console.log("start " + this.config.start.link)
             this.config.start.audioSource.started = true;
 
             this.started = true;
@@ -99,6 +101,7 @@ class CarSoundEngine {
             for (let o in this.config) {
                 if ('audioSource' in this.config[o]) {
                     this.config[o].audioSource.stop();
+                    console.log("stop " + this.config[o].link)
                     this.config[o].started = false;
                     delete this.config[o].audioSource;
                 } else if (this.config[o] instanceof Array) {
@@ -106,6 +109,7 @@ class CarSoundEngine {
                         if ('audioSource' in el) {
                             el.started = false;
                             el.audioSource.stop();
+                            console.log("stop " + el.link);
                             delete el.audioSource;
                         }
                     });
@@ -117,6 +121,7 @@ class CarSoundEngine {
                     resolve();
                 });
                 this.config.stop.audioSource.start();
+                console.log("start " + this.config.stop.link)
                 this.config.stop.audioSource.started = true;
                 this.started = false;
             } else {
@@ -168,6 +173,7 @@ class CarSoundEngine {
                             this.createSound(el);
                             el.gainNode.gain.value = el.speed.volume[0][1];
                             el.audioSource.start();
+                            console.log("start " + el.link)
                             el.started = true;
                         }
                     } else {
@@ -177,11 +183,13 @@ class CarSoundEngine {
                             } else {
                                 this.createSound(el);
                                 el.audioSource.start();
+                                console.log("start " + el.link)
                                 el.started = true;
                             }
                         }  else {
                             if (el.started) {
                                 el.audioSource.stop();
+                                console.log("stop " + el.link)
                                 el.started = false;
                             }
                         }
@@ -189,6 +197,7 @@ class CarSoundEngine {
                 } else {
                     if (el.started) {
                         el.audioSource.stop();
+                        console.log("stop " + el.link)
                         el.started = false;
                     }
                 }
