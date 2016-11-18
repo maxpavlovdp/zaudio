7af9962a8c51b0095f7ceaa960891b1afee3c8fd
walakos <walakos@gmail.com>
2016-11-18 20:05:52 +0200

    BOOST-98 V3 first pack (I'm on fire))))

--- webapp/src/components/ZEBoosterApp.jsx
+++ webapp/src/components/ZEBoosterApp.jsx
@@ -5,7 +5,13 @@ import CarSoundEngine from './../CarSoundEngine.js'
 
 let engineOnSampleV3 = require("../sounds/v3/engineOnSampleV3.ogg"),
     engineOffSampleV3 = require("../sounds/v3/engineOffSampleV3.ogg"),
-    idlingLoopV3 = require("../sounds/v3/idlingLoopV3.ogg");
+    idlingLoopV3 = require("../sounds/v3/idlingLoopV3.ogg"),
+    accelerationV3 = require("../sounds/v3/accelerationLoopV3.ogg"),
+    Loop60V3 = require("../sounds/v3/Loop60V3.ogg"),
+    Loop100V3 = require("../sounds/v3/Loop100V3.ogg"),
+    Loop160V3 = require("../sounds/v3/Loop160V3.ogg"),
+    recuperationLoopV3 = require("../sounds/v3/recuperationLoopV3.ogg");
+
 var soundSchemeV3 = {
     start: {
         link: engineOnSampleV3
@@ -15,10 +21,51 @@ var soundSchemeV3 = {
             link: idlingLoopV3,
             speed: {
                 margins: [0, 250],
-                volume: [[0, 0.4], [150, 1]]
+                volume: [[0, 1], [150, 1.4]]
             },
             loop: true
         },
+        {
+            link: accelerationV3,
+            speed: {
+                margins: [0, 250],
+                volume: [[0, 0.01], [30, 1]]
+            },
+            loop: true
+        },
+        {
+            link: Loop60V3,
+            speed: {
+                margins: [60, 250],
+                volume: [[60, 0.01], [75, 0.7]]
+            },
+            loop: true
+        },
+        {
+            link: Loop100V3,
+            speed: {
+                margins: [100, 250],
+                volume: [[100, 0.01], [130, 1.3]]
+            },
+            loop: true
+        },
+        {
+            link: Loop160V3,
+            speed: {
+                margins: [160, 250],
+                volume: [[160, 0.01], [170, 1.7]]
+            },
+            loop: true
+        },
+        {
+            link: recuperationLoopV3,
+            speed: {
+                margins: [0, 250],
+                volume: [[0, 0.01], [30, 4], [250, 5.5]]
+            },
+            recuperation: true,
+            loop: true
+        }
     ],
     stop: {
         link: engineOffSampleV3
