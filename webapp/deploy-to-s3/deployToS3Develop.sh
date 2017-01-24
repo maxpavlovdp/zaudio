65dce417693a18603be105be1c9a55d81858e16f
Max Pavlov <maxpavlov.dp@gmail.com>
2017-01-24 16:56:07 +0100

    BOOST-151 docker image for pipeline build

--- webapp/deploy-to-s3/deployToS3Develop.sh
+++ webapp/deploy-to-s3/deployToS3Develop.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
-aws s3 help
+set -x
+cd "$(dirname "$0")"
+
+rm -rf ~/.aws
+mkdir ~/.aws
+
+echo "[default]" >> ~/.aws/credentials
+echo "aws_access_key_id=AKIAILWAFXOS46EG6SXQ" >> ~/.aws/credentials
+echo "aws_secret_access_key=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" >> ~/.aws/credentials
+
+echo "[default]" >> ~/.aws/config
+echo "region=eu-central-1" >> ~/.aws/config
+echo "output=json" >> ~/.aws/config
+
+cp ../../src/config/prodConfig.js ../../public/config.js
+aws s3 sync --delete ../../public/* s3://zas-develop-branch
