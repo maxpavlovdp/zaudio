c6d1792ef30c880ab1047d85844b5e6a780fd2d0
Max Pavlov <maxpavlov.dp@gmail.com>
2017-03-22 17:42:31 +0100

    BOOST-132 added model s specs

---
+++ webapp/src/config/carParams.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Max Pavlov on 3/22/17.
+ */
+
+//https://en.wikipedia.org/wiki/Tesla_Model_S#Specifications
+export default teslaModelS = {
+    weight: 2200, // kg
+    maxMotorPower: 581, //kWt
+    dragCoef: 0.24,
+    frontArea: 2.34 // m2
+}
+
+
