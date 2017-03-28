b58251b1498a749c238f175cc5169f2be9533312
Max Pavlov <maxpavlov.dp@gmail.com>
2017-03-28 13:53:10 +0200

    BOOST-134 Fixed tests

--- webapp/src/reducers/ssButtonToggle.spec.js
+++ webapp/src/reducers/ssButtonToggle.spec.js
@@ -1,32 +1,29 @@
-import reducers from '../reducers';
+import ssButtonToggle from '../reducers/ssButtonToggle';
 
 describe('ssButtonToggle reducer tests', () => {
 
     it('should register start/stop button in store', () => {
         let state;
         let action = {type: 'REGISTER_START_BTN', newBtn: {name: 'V4', isOn: false}};
-        let actionResultInStore = {ssButtonToggle: [{name: 'V4', isOn: false}]};
+        let actionResultInStore = [{name: 'V4', isOn: false}];
 
-        state = reducers({ssButtonToggle: {}},
+        state = ssButtonToggle({ssButtonToggle: {}},
             action);
         expect(state).toEqual(actionResultInStore);
     })
 
     it('should change button isOn state when btn 1 clicked', () => {
-        let state;
-        state = reducers({
-                ssButtonToggle: [
-                    {name: 'Btn 1', isOn: false},
-                    {name: 'Btn 2', isOn: false}
-                ]
-            },
+        let state = ssButtonToggle([
+                {name: 'Btn 1', isOn: false},
+                {name: 'Btn 2', isOn: false}
+            ]
+            ,
             {type: 'ENGINE_ON_OFF', clickedBtn: {name: 'Btn 2', isOn: true}}
         );
-        expect(state).toEqual({
-            ssButtonToggle: [
+        expect(state).toEqual([
                 {name: 'Btn 1', isOn: false},
                 {name: 'Btn 2', isOn: true}
             ]
-        });
+        );
     })
 });
