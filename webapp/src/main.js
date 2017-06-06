09680470146aa8bf687a76f6e8bda0be99c9634e
Max Pavlov <maxpavlov.dp@gmail.com>
2017-06-06 16:36:24 +0200

    BOOST-181 removed react router

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {Router, Route, hashHistory} from 'react-router';
+import ReactDOM from 'react-dom';
 
 import ZEBoosterApp from './components/ZEBoosterApp.jsx';
-import FocusGroupPage from './components/pages/focusGroupPage.jsx';
 
-render((
-        <Router history={hashHistory}>
-            <Route path="/" component={ZEBoosterApp}/>
-            <Route path="/focusGroup" component={FocusGroupPage}/>
-        </Router>
-    ), document.getElementById('content')
+ReactDOM.render(
+    <ZEBoosterApp />,
+    document.getElementById('content')
 );
