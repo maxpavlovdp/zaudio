7e6344ba59e4d05509f22b82e334531823e14a35
walakos <walakos@gmail.com>
2017-06-19 16:39:18 +0300

    BOOST-156 - Daily connection bug fixing, production of additional acceleration and recuperation sound BOOST-190 - Fixing Daily EngineOn sample with more calm character of upper sound layer

--- webapp/src/sounds/daily/schemeDaily.jsx
+++ webapp/src/sounds/daily/schemeDaily.jsx
@@ -1,6 +1,8 @@
 let engOn = require("./DailyV1EngOn.ogg"),
     engOff = require("./DailyV1EngOff.ogg"),
-    idling = require("./DailyV1IdlingLoop.ogg")
+    idling = require("./DailyV1IdlingLoop.ogg"),
+    acc60240 = require("./DailyV160240.ogg"),
+    recuperation = require("./DailyV1Rec.ogg");
 
 let schemeDaily = {
     name: "Daily",
@@ -22,7 +24,35 @@ let schemeDaily = {
                 // power: [[0, 0], [15000, 0.5], [35000, 0.7]]
             },
             loop: true
-        }
+        },
+        {
+            link: acc60240,
+            margins: {
+                speed: [55, 241]
+            },
+            pbr: {
+                speed: [[55, 0], [60, 0.5], [100, 1.0], [160, 1.0], [240, 1.7]]
+            },
+            volume: {
+                speed: [[55, 0], [60, 0.5], [240, 0.9]],
+            },
+            loop: true
+        },
+        {
+            link: recuperation,
+            margins: {
+                speed: [0, 241]
+            },
+            pbr: {
+                speed: [[0, 0], [10, 1.1], [20, 1.3], [30, 1.5], [40, 1.2], [90, 0.7], [140, 1.2], [240, 1]]
+            },
+            volume: {
+                // recuperationPower: [[0, 0], [20000, 1], [70000, 2]]
+                speed:[[0, 0], [5, 1.0], [10, 1.0], [20, 1.1], [30, 0.9], [40, 0.7], [90, 0.5], [190, 1.5], [240, 0.2]]
+            },
+            recuperation: true,
+            loop: true
+        },
     ],
     stop: {
         link: engOff
