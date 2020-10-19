19c2292c6184b252ae7e151c8d9576fb0f8d8409
Motorboat <eskimorolerr@gmail.com>
2020-10-19 14:34:16 +0300

    Deploy using run-build-and-deploy-to-git-hub-pages.sh

--- run-build-and-deploy-to-git-hub-pages.sh
+++ run-build-and-deploy-to-git-hub-pages.sh
@@ -4,6 +4,11 @@ sh ./webapp/runBuild.sh
 
 cp -R ./webapp/public/* ./docs/
 cp -R ./webapp/public/assets ./docs/sketches/
+cp -R ./webapp/public/assets ./docs/buySound
+cp -R ./webapp/public/assets ./docs/contacts
+cp -R ./webapp/public/assets ./docs/forCommunity
+cp -R ./webapp/public/assets ./docs/forInvestors
+cp -R ./webapp/public/assets ./docs/mission
 
 git add -A
 git commit -m "Deploy using run-build-and-deploy-to-git-hub-pages.sh"
