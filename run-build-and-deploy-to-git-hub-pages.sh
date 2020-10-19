2dfaa47adfe934664d850e9c2379c89284008195
Motorboat <eskimorolerr@gmail.com>
2020-10-19 14:29:03 +0300

    Deploy using run-build-and-deploy-to-git-hub-pages.sh

--- run-build-and-deploy-to-git-hub-pages.sh
+++ run-build-and-deploy-to-git-hub-pages.sh
@@ -1,7 +1,9 @@
 cd "$(dirname "$0")"
 sh ./webapp/srcJekyll/deploy.sh
 sh ./webapp/runBuild.sh
+
 cp -R ./webapp/public/* ./docs/
+cp -R ./webapp/public/assets ./docs/sketches/
 
 git add -A
 git commit -m "Deploy using run-build-and-deploy-to-git-hub-pages.sh"
